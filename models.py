239ca0ec68e3214edba28d22ac2924cff268ed8d
Pranav Ramakrishnan <94326046+pvr5594@users.noreply.github.com>
2022-06-10 05:34:51 -0400

    Update models.py

--- models.py
+++ models.py
@@ -5,21 +5,35 @@ from flask_login import UserMixin, LoginManager
 db = SQLAlchemy()
 login = LoginManager()
 
-
 class UserModel(UserMixin, db.Model):
-    __tablename__ = 'users'
+        __tablename__ = "teachers"
 
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(20), nullable=False)
+        id = db.Column(db.Integer, primary_key = True)
+        username = db.Column(db.String(20), unique = True, nullable = False)
+        email = db.Column(db.String(80), unique = True, nullable = False)
+        password_hash = db.Column(db.String(255), nullable = False)
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        def set_password(self, password):
+                self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+        def check_password(self, password):
+                return check_password_hash(self.password_hash, password)
 
+class StudentModel(UserMixin, db.Model):
+        __tablename__ = "students"
+
+        id = db.Column(db.Integer, primary_key = True)
+        username = db.Column(db.String(20), unique = True, nullable = False)
+        password_hash = db.Column(db.String(255), nullable = False)
+
+        def set_password(self, password):
+                self.password_hash = generate_password_hash(password)
+
+        def check_password(self, password):
+                return check_password_hash(self.password_hash, password)
 
 @login.user_loader
 def load_user(id):
-    return UserModel.query.get(int(id))
+        return UserModel.query.get(int(id))
+        return StudentModel.query.get(int(id))
+
